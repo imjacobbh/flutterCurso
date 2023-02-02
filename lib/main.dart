a39c0975ee1676f79634b4a78a29776c2d98f4a4
jacobFinloop <jacob.bonilla@finloop.com.mx>
2023-02-02 11:22:15 -0600

    animatedContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:untitled/pages/PaginaHome.dart';
 import 'package:untitled/pages/PaginaUsers.dart';
@@ -12,33 +14,46 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _paginaActual = 0;
+  double _width = 100.0;
+  double _height = 100.0;
+  Color _color = Colors.blue;
+  BorderRadius _borderRadius = BorderRadius.circular(10);
 
-  List<Widget> _paginas = [PaginaHome(), PaginaUsers()];
+  void _cambiarContainer() {
+    final random = Random();
+    _width = random.nextInt(350).toDouble();
+    _height = random.nextInt(350).toDouble();
+    _color = Color.fromRGBO(random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+    _borderRadius = BorderRadius.circular(random.nextInt(20).toDouble());
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Material App',
         home: Scaffold(
-          appBar: AppBar(
-            title: Text("Material App bar"),
-          ),
-          body: _paginas[_paginaActual],
-          bottomNavigationBar: BottomNavigationBar(
-            onTap: (index) {
-              setState(() {
-                _paginaActual = index;
-              });
-            },
-            currentIndex: _paginaActual,
-            items: [
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.supervised_user_circle_sharp),
-                  label: "contacts")
-            ],
-          ),
-        ));
+            appBar: AppBar(
+              title: Text("Material App bar"),
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Center(
+                  child: AnimatedContainer(
+                    curve: Curves.fastOutSlowIn,
+                    duration: Duration(seconds: 1),
+                    width: _width,
+                    height: _height,
+                    decoration: BoxDecoration(
+                        color: _color, borderRadius: _borderRadius),
+                  ),
+                )),
+                ElevatedButton(
+                    onPressed: _cambiarContainer,
+                    child: Text("Cambiar Container"))
+              ],
+            )));
   }
 }
